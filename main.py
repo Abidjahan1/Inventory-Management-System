6d7ccf1ec16a9f6c5ad72107a60a7aca076132a5
abidjahan1 <abidjahan1993@gmail.com>
2024-08-26 18:43:16 +0600

    this is the first commit

---
+++ main.py
@@ -0,0 +1,70 @@
+import csv
+class Item:
+    #class attribute
+    pay_rate = 0.8 # The pay rate afer 20% discount
+    all = []
+    
+    
+    def __init__(self, name: str,  price: int,quantity = 0):
+        # Run validation to the recieved arguments
+        assert price >= 0,f"Price {price}is not greater than or equal to zero"
+        assert quantity >= 0
+        
+        #Assign to self object
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        
+        #Actinos to execute
+        Item.all.append(self)
+    
+    def calculate_total_price(self):
+        return self.quantity * self.price
+    
+    
+    def apply_discount(self):
+        self.price = self.price * self.pay_rate
+        
+
+    #decorator for class method
+    @classmethod
+    def instanciate_from_csv(cls):
+        with open('items.csv','r') as f:
+            reader = csv.DictReader(f)
+            items = list(reader)
+            
+        for item in items:
+            Item(
+                name=item.get("name"),
+                price=float(item.get("price")),
+                quantity=float(item.get("quantity")),
+
+            )
+    #control objects 
+    def __repr__(self) -> str:
+        return f"Item('{self.name}','{self.price}','{self.quantity}')"
+
+item1 = Item("Phone", 100, 1)
+item2 = Item("Laptop", 1000, 3)
+item3 = Item("Cable", 10, 5)
+item4 = Item("Mouse", 50, 5)
+item5 = Item("Keyboard", 75, 5)  
+
+Item.instanciate_from_csv()
+print(Item.all)
+
+
+# print(Item.all)
+
+# # print(item1.calculate_total_price())
+
+# item2.apply_discount()
+# print(item2.price)
+
+# item1.pay_rate = 0.7
+# item1.apply_discount()
+# print(item1.price)
+
+
+# print(Item.__dict__)# magic attributes for class level
+# print(item1.__dict__) # magic attributes for instance level
