5abf4293f07e04011db771a30c30d438a0d18ca8
abidjahan1 <abidjahan1993@gmail.com>
2024-08-26 18:44:23 +0600

    this is the second commit

--- main.py
+++ main.py
@@ -44,11 +44,7 @@ class Item:
     def __repr__(self) -> str:
         return f"Item('{self.name}','{self.price}','{self.quantity}')"
 
-item1 = Item("Phone", 100, 1)
-item2 = Item("Laptop", 1000, 3)
-item3 = Item("Cable", 10, 5)
-item4 = Item("Mouse", 50, 5)
-item5 = Item("Keyboard", 75, 5)  
+  
 
 Item.instanciate_from_csv()
 print(Item.all)
